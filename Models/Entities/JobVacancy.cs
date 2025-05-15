f52fc8dec30863dcf44bb3dbef3929309491e3ef
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2025-05-15 13:23:55 +0300

    perf: add new vacancy entity

---
+++ Models/Entities/JobVacancy.cs
@@ -0,0 +1,21 @@
+using Aeon_Web.Models.Entities.Resume;
+
+namespace Aeon_Web.Models.Entities;
+
+public class JobVacancy
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    
+    public byte DifficultyLevel { get; set; }
+
+    public List<string> SkillsRequired { get; set; } = [];
+    
+    public DateTime PostedDate { get; set; } = DateTime.UtcNow;
+    
+    public DateTime? ExpirationDate { get; set; }
+    
+    public ContactInfo Contact { get; set; } = new();
+}
